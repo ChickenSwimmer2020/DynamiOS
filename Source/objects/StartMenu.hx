a9e1dd318b66086bad7d5d5087f9fd1e35856db7
ChickenSwimmer2020 <105177647+ChickenSwimmer2020@users.noreply.github.com>
2025-03-26 13:43:57 -0400

    ALMOST DONE

--- Source/objects/StartMenu.hx
+++ Source/objects/StartMenu.hx
@@ -1,5 +1,7 @@
 package objects;
 
+import objects.shell.ShellMenu;
+
 class StartMenu extends FlxSpriteGroup{
     public var open:Bool = false;
     public var tweening:Bool = false;
@@ -7,13 +9,53 @@ class StartMenu extends FlxSpriteGroup{
     public var background:FlxSprite;
     public var background2:FlxSprite;
 
+    public var powericon:FlxText;
+    public var powerButton:FlxButton;
+    public var shellToggle:Bool = false;
+
     public function new(x:Float, y:Float){
         super(x, y);
 
         background = new FlxSprite(x, y).makeGraphic(500, 69 * 10, FlxColor.fromString('0x1f1f1f')); //hehe, nice.
         background2 = new FlxSprite(x, y + 590).makeGraphic(500, 100, FlxColor.fromString('0x181818'));
+        powericon = new FlxText(400, 630, 32, '', 32, true);
+        powerButton = new FlxButton(400, 630, '', ()->{toggleShell('power'); shellToggle = !shellToggle; });
+        powerButton.loadGraphic('assets/ui/buttonSQR.png', true, 20, 20);
+        powerButton.scale.set(2.1, 2.1);
+        powerButton.updateHitbox();
+        powerButton.visible = false;
+        powericon.font = 'assets/fonts/SEGMDL2.TTF';
         add(background);
         add(background2);
+
+        add(powericon);
+        add(powerButton);
+
+        var shellmnu:ShellMenu = new ShellMenu(0, 0, {
+        tabs: 4, 
+        parent: powerButton,
+        tabNames: [
+            'Lock',
+            'Sleep',
+            'Shut Down',
+            'Restart'
+        ],
+        tabFunctions: [
+            ()->{trace('Locked');},
+            ()->{trace('Sleeped');},
+            ()->{trace('Shut Downed');},
+            ()->{trace('Restarted');}
+        ],
+        alignment: 'TOP',
+        tabIconsTXT: [
+            ["character" => "", "font" => "assets/fonts/SEGMDL2.TTF"],
+            ["character" => "", "font" => "assets/fonts/SEGMDL2.TTF"],
+            ["character" => "", "font" => "assets/fonts/SEGMDL2.TTF"],
+            ["character" => "", "font" => "assets/fonts/SEGMDL2.TTF"]
+        ]
+        });
+        add(shellmnu);
+        
 
         y = y - 100; 
     }
@@ -32,6 +74,9 @@ class StartMenu extends FlxSpriteGroup{
             tweening = true;
             FlxTween.tween(this, {y: y + 700}, 0.2, { ease: FlxEase.smootherStepIn, onComplete: function(_:FlxTween) { visible = false; tweening = false; } });
         }
+    }
+
+    private function toggleShell(type:String){
     }
 
 }
